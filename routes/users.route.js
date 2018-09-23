9476506ee2c9eff2c266e529516d74f9301c29f6
mymymy1303 <thehuy160897@gmail.com>
2018-09-23 21:59:51 +0700

    Add GET request handle to get all users

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,20 +1,19 @@
 import express from 'express';
-import userController from './../controllers/users.controller'
-import User from './../models/user.model'
+import User from './../models/user.model';
 
 
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-	let query = User.find();
+	let query = User.find()
+		.sort({
+			username: 1
+		});
 
 	query.exec()
 		.then(docs => {
-			res.render('users', {
-				title: 'Users List',
-				data: docs
-			});
+			res.json(docs)
 		})
 		.catch(err => console.err(err))
 });
